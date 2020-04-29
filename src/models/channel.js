791703b61564e08e05cd61207b0bf138032bb4af
Steven Chester <steven.chester@industryweapon.com>
2020-04-29 09:59:27 -0400

    more error handling

--- src/models/channel.js
+++ src/models/channel.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import config from "../config";
 
-export const getChannelByDevice = async deviceId => {
+export const getChannelByDevice = async (deviceId) => {
   const response = await axios
     .post(
       `${config.apiEndpoint}/scs:RPC.wdGetCurrentChannel`,
@@ -13,7 +13,17 @@ export const getChannelByDevice = async deviceId => {
     throw new Error("The device is not configured correctly.");
   }
 
+  if (response.indexOf("ERR:") > -1) {
+    const [, error] = response.split("ERR:");
+
+    throw new Error(error);
+  }
+
   const channelInfo = response.split(" ");
+
+  if (!channelInfo || !channelInfo[4]) {
+    throw new Error("Missing channel info");
+  }
 
   const [width, height] = channelInfo[4].split("-");
 
@@ -23,7 +33,7 @@ export const getChannelByDevice = async deviceId => {
     height: parseFloat(height),
     version: channelInfo[1],
     slideDelays: channelInfo[3].split(","),
-    slideCount: parseInt(channelInfo[2]) + 1
+    slideCount: parseInt(channelInfo[2]) + 1,
   };
 
   return channel;
